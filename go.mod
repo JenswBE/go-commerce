e28915be41988cdda56e15db5c645e1a1e09feae
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2021-05-10 22:20:45 +0200

    Fix short ID (base58)

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/JenswBE/go-commerce
 go 1.15
 
 require (
+	github.com/btcsuite/btcutil v1.0.2
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.1
 	github.com/google/uuid v1.2.0
-	github.com/itchyny/base58-go v0.1.0
 	github.com/stretchr/testify v1.4.0
 )
--- pkg/shortid/base58.go
+++ pkg/shortid/base58.go
@@ -1,43 +1,26 @@
 package shortid
 
 import (
-	"fmt"
-
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/google/uuid"
-	"github.com/itchyny/base58-go"
 )
 
 // Base58 shortens ID using a Flickr base58 encoding
-type Base58 struct {
-	encoding *base58.Encoding
-}
+type Base58 struct{}
 
 // NewBase58Service creates a new base58 short ID service
 func NewBase58Service() *Base58 {
-	return &Base58{
-		encoding: base58.FlickrEncoding,
-	}
+	return &Base58{}
 }
 
 // Encode converts an UUID to a short ID
 func (b *Base58) Encode(input uuid.UUID) string {
-	// Can't fail with enforced UUID input
-	inputString := fmt.Sprintf("%x", input[:])
-	encoded, err := b.encoding.Encode([]byte(inputString))
-	if err != nil {
-		panic(err.Error())
-	}
+	encoded := base58.Encode(input[:])
 	return string(encoded)
 }
 
 // Decode converts a short ID to an UUID
 func (b *Base58) Decode(input string) (uuid.UUID, error) {
-	// Decode short ID
-	decoded, err := b.encoding.Decode([]byte(input))
-	if err != nil {
-		return uuid.Nil, err
-	}
-
-	// Convert to UUID
+	decoded := base58.Decode(input)
 	return uuid.FromBytes(decoded)
 }
