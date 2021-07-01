7ef70066c33157f6b4e4b36b464577c256a0e83f
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2021-07-01 21:38:50 +0200

    Fix inconsistent trailing slash

--- api/handler/product_common.go
+++ api/handler/product_common.go
@@ -8,31 +8,31 @@ import (
 
 func AddProductReadRoutes(rg *gin.RouterGroup, p *presenter.Presenter, service product.Usecase) {
 	groupCategories := rg.Group("/categories")
-	groupCategories.GET("/", listCategories(p, service))
+	groupCategories.GET("", listCategories(p, service))
 	groupCategories.GET("/:id", getCategory(p, service))
 
 	groupManufacturers := rg.Group("/manufacturers")
-	groupManufacturers.GET("/", listManufacturers(p, service))
+	groupManufacturers.GET("", listManufacturers(p, service))
 	groupManufacturers.GET("/:id", getManufacturer(p, service))
 
 	groupProducts := rg.Group("/products")
-	groupProducts.GET("/", listProducts(p, service))
+	groupProducts.GET("", listProducts(p, service))
 	groupProducts.GET("/:id", getProduct(p, service))
 }
 
 func AddProductWriteRoutes(rg *gin.RouterGroup, p *presenter.Presenter, service product.Usecase) {
 	groupCategories := rg.Group("/categories")
-	groupCategories.POST("/", createCategory(p, service))
+	groupCategories.POST("", createCategory(p, service))
 	groupCategories.PUT("/:id", updateCategory(p, service))
 	groupCategories.DELETE("/:id", deleteCategory(p, service))
 
 	groupManufacturers := rg.Group("/manufacturers")
-	groupManufacturers.POST("/", createManufacturer(p, service))
+	groupManufacturers.POST("", createManufacturer(p, service))
 	groupManufacturers.PUT("/:id", updateManufacturer(p, service))
 	groupManufacturers.DELETE("/:id", deleteManufacturer(p, service))
 
 	groupProducts := rg.Group("/products")
-	groupProducts.POST("/", createProduct(p, service))
+	groupProducts.POST("", createProduct(p, service))
 	groupProducts.PUT("/:id", updateProduct(p, service))
 	groupProducts.DELETE("/:id", deleteProduct(p, service))
 }
