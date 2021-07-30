156e3b2113f69e0d7b2af67ae468ee8f94042a51
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2021-07-30 22:05:47 +0200

    Add unit tests for parseFilesFromMultipart

--- api/handler/product_common_test.go
+++ api/handler/product_common_test.go
@@ -1,11 +1,50 @@
 package handler
 
 import (
+	"bytes"
+	"mime/multipart"
+	"net/http"
 	"testing"
 
 	"github.com/JenswBE/go-commerce/utils/imageproxy"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_parseFilesFromMultipart_Success(t *testing.T) {
+	// Setup test
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	test1, _ := writer.CreateFormFile("file", "test1.jpg")
+	test1.Write([]byte("test-1-jpg"))
+	test2, _ := writer.CreateFormFile("file", "test2.png")
+	test2.Write([]byte("test-2-png"))
+	writer.Close()
+	req, _ := http.NewRequest("", "", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Call function
+	files, err := parseFilesFromMultipart(req)
+
+	// Assert results
+	require.NoError(t, err)
+	expected := map[string][]byte{
+		"test1.jpg": []byte("test-1-jpg"),
+		"test2.png": []byte("test-2-png"),
+	}
+	require.Equal(t, expected, files)
+}
+
+func Test_parseFilesFromMultipart_Failure(t *testing.T) {
+	// Setup test
+	req, _ := http.NewRequest("", "", bytes.NewBufferString("invalid"))
+
+	// Call function
+	files, err := parseFilesFromMultipart(req)
+
+	// Assert results
+	require.Error(t, err)
+	require.Nil(t, files)
+}
 
 func Test_parseImageConfigParams_AllParamsSet_Success(t *testing.T) {
 	// Setup test
