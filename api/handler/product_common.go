8320a80092469faee22d6782cce4e987900370f5
Jens Willemsens <jens@jensw.be>
2021-08-10 21:52:16 +0200

    Use trailing slash

--- api/handler/product_common.go
+++ api/handler/product_common.go
@@ -25,41 +25,41 @@ func NewProductHandler(p *presenter.Presenter, service product.Usecase) *Product
 
 func (h *ProductHandler) RegisterReadRoutes(rg *gin.RouterGroup) {
 	groupCategories := rg.Group("/categories")
-	groupCategories.GET("", h.listCategories)
-	groupCategories.GET("/:id", h.getCategory)
+	groupCategories.GET("/", h.listCategories)
+	groupCategories.GET("/:id/", h.getCategory)
 
 	groupManufacturers := rg.Group("/manufacturers")
-	groupManufacturers.GET("", h.listManufacturers)
-	groupManufacturers.GET("/:id", h.getManufacturer)
+	groupManufacturers.GET("/", h.listManufacturers)
+	groupManufacturers.GET("/:id/", h.getManufacturer)
 
 	groupProducts := rg.Group("/products")
-	groupProducts.GET("", h.listProducts)
-	groupProducts.GET("/:id", h.getProduct)
-	groupProducts.GET("/:id/images", h.listProductImages)
+	groupProducts.GET("/", h.listProducts)
+	groupProducts.GET("/:id/", h.getProduct)
+	groupProducts.GET("/:id/images/", h.listProductImages)
 }
 
 func (h *ProductHandler) RegisterWriteRoutes(rg *gin.RouterGroup) {
 	groupCategories := rg.Group("/categories")
-	groupCategories.POST("", h.createCategory)
-	groupCategories.PUT("/:id", h.updateCategory)
-	groupCategories.DELETE("/:id", h.deleteCategory)
-	groupCategories.PUT("/:id/image", h.upsertCategoryImage)
-	groupCategories.DELETE("/:id/image", h.deleteCategoryImage)
+	groupCategories.POST("/", h.createCategory)
+	groupCategories.PUT("/:id/", h.updateCategory)
+	groupCategories.DELETE("/:id/", h.deleteCategory)
+	groupCategories.PUT("/:id/image/", h.upsertCategoryImage)
+	groupCategories.DELETE("/:id/image/", h.deleteCategoryImage)
 
 	groupManufacturers := rg.Group("/manufacturers")
-	groupManufacturers.POST("", h.createManufacturer)
-	groupManufacturers.PUT("/:id", h.updateManufacturer)
-	groupManufacturers.DELETE("/:id", h.deleteManufacturer)
-	groupManufacturers.PUT("/:id/image", h.upsertManufacturerImage)
-	groupManufacturers.DELETE("/:id/image", h.deleteManufacturerImage)
+	groupManufacturers.POST("/", h.createManufacturer)
+	groupManufacturers.PUT("/:id/", h.updateManufacturer)
+	groupManufacturers.DELETE("/:id/", h.deleteManufacturer)
+	groupManufacturers.PUT("/:id/image/", h.upsertManufacturerImage)
+	groupManufacturers.DELETE("/:id/image/", h.deleteManufacturerImage)
 
 	groupProducts := rg.Group("/products")
-	groupProducts.POST("", h.createProduct)
-	groupProducts.PUT("/:id", h.updateProduct)
-	groupProducts.DELETE("/:id", h.deleteProduct)
-	groupProducts.POST("/:id/images", h.addProductImages)
-	groupProducts.PUT("/:id/images/:image_id", h.updateProductImage)
-	groupProducts.DELETE("/:id/images/:image_id", h.deleteProductImage)
+	groupProducts.POST("/", h.createProduct)
+	groupProducts.PUT("/:id/", h.updateProduct)
+	groupProducts.DELETE("/:id/", h.deleteProduct)
+	groupProducts.POST("/:id/images/", h.addProductImages)
+	groupProducts.PUT("/:id/images/:image_id/", h.updateProductImage)
+	groupProducts.DELETE("/:id/images/:image_id/", h.deleteProductImage)
 }
 
 func parseFilesFromMultipart(req *http.Request) (map[string][]byte, error) {
